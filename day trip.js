ea19875e6a51bd2e5488ac9677a718a583ed6e0f
unknown <kylespinaio@hotmail.com>
2020-06-15 22:08:21 -0400

    Defined variables and added arrays to day trip.js

--- day trip.js
+++ day trip.js
@@ -3,16 +3,34 @@
 // Technologies: JavaScript, HTML, CSS, Bootstrap
 // Features: 
 // (5 points): As a developer, I want to make good, consistent commits.
+
 // (5 points): As a user, I want a destination to be randomly selected for my day trip.
+
+let destinationCity = ["Dallas", "Orlando", "Pheonix", "Twin Falls", "Nashville", "Savannah", "Biloxi", "Cheyanne" ];
+
+
 // (5 points): As a user, I want a restaurant to be randomly selected for my day trip.
+
+let resturant = ["In-n-Out Burger", "Milwaukee Burger Company", "Franklin's BBQ", "The Burnt Lemon Grill", "4 Rivers BBQ", "The Fat Snook", "Junior's Cheesecake", "Rising Tide"];
+
+
+
 // (5 points): As a user, I want a mode of transportation to be randomly selected for my day trip.
+
+
+let typeOfTransportation = ["Car", "Truck", "Bicycle", "Walk", "Train", "Plane", "Bus", "Uber" ];
+
+
 // (5 points): As a user, I want a form of entertainment to be randomly selected for my day trip.
+
+let entertainment = ["Playing cards, Counting lisence plates", "Singing", "Sleeping", "Satching movies"];
+
+
 // (15 points): As a user, I want to be able to randomly re-select a destination, restaurant, mode of transportation, and/or form of entertainment if I don’t like one or more of those things.
 // (10 points): As a user, I want to be able to confirm that my day trip is “complete” once I like all of the random selections.
 // (10 points): As a user, I want to display my completed trip in the console.
 // (5 points): As a developer, I want all of my functions to have a Single Responsibility. Remember, each function should do just one thing!
 
-let destination = ["Dallas", "Orlando", "Pheonix", "Nashville", "Savannah", "Biloxi", "Cheyanne" ]
-let resturante = ["In-n-Out Burger"]
 
-console.log("check check")
+function daytrip()
+
