5988dfe5e375a1974e860c3bb80c07569702e082
unknown <kylespinaio@hotmail.com>
2020-06-15 22:52:02 -0400

    Added random variables, if/else if statement and began outline of random function

--- day trip.js
+++ day trip.js
@@ -8,12 +8,15 @@
 
 let destinationCity = ["Dallas", "Orlando", "Pheonix", "Twin Falls", "Nashville", "Savannah", "Biloxi", "Cheyanne" ];
 
+let randomDestinationCity = destinationCity[Math.floor(Math.random()*destinationCity.length)]
+
+
 
 // (5 points): As a user, I want a restaurant to be randomly selected for my day trip.
 
 let resturant = ["In-n-Out Burger", "Milwaukee Burger Company", "Franklin's BBQ", "The Burnt Lemon Grill", "4 Rivers BBQ", "The Fat Snook", "Junior's Cheesecake", "Rising Tide"];
 
-
+let randomeRestruant = resturant[Math.floor(Math.random()*resturant.length)]
 
 // (5 points): As a user, I want a mode of transportation to be randomly selected for my day trip.
 
@@ -23,7 +26,7 @@ let typeOfTransportation = ["Car", "Truck", "Bicycle", "Walk", "Train", "Plane",
 
 // (5 points): As a user, I want a form of entertainment to be randomly selected for my day trip.
 
-let entertainment = ["Playing cards, Counting lisence plates", "Singing", "Sleeping", "Satching movies"];
+let entertainment = ["Playing cards, Counting license plates", "Singing", "Sleeping", "Watching movies"];
 
 
 // (15 points): As a user, I want to be able to randomly re-select a destination, restaurant, mode of transportation, and/or form of entertainment if I don’t like one or more of those things.
@@ -31,6 +34,30 @@ let entertainment = ["Playing cards, Counting lisence plates", "Singing", "Sleep
 // (10 points): As a user, I want to display my completed trip in the console.
 // (5 points): As a developer, I want all of my functions to have a Single Responsibility. Remember, each function should do just one thing!
 
+  let userInput = prompt("Please select Yes or No. Would you like to go on a day trip?: ");
 
-function daytrip()
+  function daytrip(){
+    if (userInput == "Yes" || "yes"){
+        console.log("Great! Calculating!")
+
+
+
+
+
+
+
+    }
+    else if (userInput == "No" || "no"){
+        console.log("Sorry to hear that. Please come back when you're ready to go on a trip.")
+    }
+    else {
+        console.log ("Please make another selection.")
+    }
+
+
+
+
+
+}
+
 
